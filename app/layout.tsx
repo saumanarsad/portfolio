e391542e0ca75f396a54f644504b3deb1be3d29d
saumanarsad <sauman.arshad@gmail.com>
2025-03-23 02:54:13 +0500

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Analytics></Analytics>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
